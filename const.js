280816ef2f1c0ff28b8c0f312743146b623bb38d
Sokolovskiy <sokolovskiy.d1993@gmail.com>
2022-01-26 18:26:41 +0300

    без портфолио

--- const.js
+++ const.js
@@ -11,3 +11,5 @@ module.exports.commands = commands
 module.exports.text1 = text1
 module.exports.text2 = text2
 module.exports.text3 = text3
+
+
--- index.js
+++ index.js
@@ -15,7 +15,8 @@ bot.command('start', async (ctx)=>{
  
     try{ //ставим в try для отслеживания ошибки
         // await используем везде где метод ctx
-        await ctx.replyWithHTML('Привет, друг! Меня зовут <b>Мистер Мопс</b>🙌🏻😇. Выбери то, что тебя интересует и я тебя со всем ознакомлю.', Markup.inlineKeyboard(
+        await ctx.replyWithHTML(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name :
+            'друг'} ! Меня зовут <b>Мистер Мопс</b>🙌🏻😇. Выбери то, что тебя интересует и я тебя со всем ознакомлю.`, Markup.inlineKeyboard(
             [
                 [Markup.button.callback('Заказать сайт', 'btn_1')],
                 [Markup.button.callback('Портфолио', 'btn_2'), Markup.button.callback('Социальные сети', 'btn_3')]
@@ -51,12 +52,106 @@ function addActionBot(name, src, text) { //УНИВЕРСАЛЬНАЯ ФУНКЦ
 }
 
 //ЗАПОЛНИТЬ ЗАЯВКУ
+const exit_keyboard = Markup.keyboard(['Выйти из заполнения']).oneTime()
+const remove_keyboard = Markup.removeKeyboard()
+
+const nameHadler = Telegraf.on('text', async ctx => {
+
+    ctx.scene.state.name = ctx.message.text
+
+    await ctx.reply('Для того, чтобы оставить заявку, оставь, пожалуйста, номер телефона, чтобы мы могли с тобой связаться😉', exit_keyboard)
+
+    return ctx.wizard.next()
+
+})
+
+const numberHadler = Telegraf.hears(/^[0-9]+$/, async ctx => {
+
+    ctx.scene.state.telephon = ctx.message.text
+
+    await ctx.reply('Если мы не дозвонимся, то отправим тебе на почту сообщение. Введи, пожалуйста, свою почту', exit_keyboard)
+
+    return ctx.wizard.next()
+})
+
+const mailHadler = Telegraf.on('text', async ctx => {
+
+    ctx.scene.state.mail = ctx.message.text
+
+    await ctx.reply('Опиши проект, который тебе необходим, мы заранее подготовим вопросы для создания твоего сайта🤔', remove_keyboard)
+
+    return ctx.wizard.next()
+
+})
+
+const descrHadler = Telegraf.on('text', async ctx => {
+
+    ctx.scene.state.descr = ctx.message.text
+    Object.assign(ctx.session, ctx.scene.state)
+
+    await ctx.replyWithHTML(
+        ` Проверь правильность внесенных данных, после чего, я отправлю их на рассмотрение.
+        <b>Обращение:</b> ${ctx.session.name}
+        <b>Твой номер:</b> ${ctx.session.telephon}
+        <b>Твой номер:</b> ${ctx.session.mail}
+        <b>Описание проекта:</b> ${ctx.session.descr}
+        `,
+        Markup.inlineKeyboard([
+        Markup.button.callback('Отправить', 'btn_info'),
+        ])
+        
+
+    )
+
+    return ctx.scene.leave()
+         
+})
+
+bot.action('btn_info', async (ctx) => {
+
+    try{
+        await ctx.answerCbQuery()//Чтобы пропадали часики загрузки в прошлой кнопке
+
+        await ctx.replyWithPhoto({
+            source: './img/desc.jpg'
+        }) 
+        
+        await ctx.replyWithHTML ( 
+        `
+Урааааа, мы прошли все шаги и заявка направлена нам!🤩
+Позднее мы свяжемся и обсудим твой проект.🤘        
+Ну а пока, ты можешь ознакомиться с нашим портфолио или посмотреть наши странички в соц.сетях.
+        `,
+        Markup.inlineKeyboard([
+            [Markup.button.callback('Портфолио', 'btn_2'), 
+            Markup.button.callback('Социальные сети', 'btn_3')]
+        
+        ]))
+
+    } catch(e) {
+        console.error(e) //если выскакивает ошибка, то она появляется в консоли
+    }
+
+})
+
+const infoScene = new WizardScene('infoScene', nameHadler, numberHadler, mailHadler, descrHadler)
+infoScene.enter(ctx => ctx.reply('Как к тебе можно обращаться?', exit_keyboard))
+
+const stage = new Stage([ infoScene ])
+stage.hears('exit', ctx => ctx.scene.leave())
+
+bot.use(session(), stage.middleware())
 
 
+bot.action('btn_1', ctx => ctx.scene.enter('infoScene'))
+
+
+
+//ПОРТФОЛИО
 addActionBot('btn_2', false, text.text2 )
 
 
-
+//СОЦ.СЕТИ
 bot.action('btn_3', async (ctx) => {
 
     try{
@@ -65,7 +160,7 @@ bot.action('btn_3', async (ctx) => {
                 source: './img/social.jpeg'
             }) 
         
-        await ctx.replyWithHTML('Посмотреть нас можно здесь', Markup.inlineKeyboard([
+        await ctx.replyWithHTML('Посмотреть наши посты в соц.сети можно здесь', Markup.inlineKeyboard([
             [
                 Markup.button.callback('Вконтакте', 'btn_vk'),
                 Markup.button.callback('Instagram', 'btn_instagram')
